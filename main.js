26c627a6c1b27dd59febb3afdd0075e42189fed6
dkstjdwns114 <sungjoon256@gmail.com>
2021-01-04 17:08:51 +0900

    연습문제

--- main.js
+++ main.js
@@ -1,89 +1,28 @@
 "use strict";
-var myName;
-myName = "Banana";
-// myName = 35; //(이미 myName에 string이 할당되었기 때문에 이후에 number 할당 불가)
-var myAge = 25;
-// myAge = '35';
-var isHeCome = false;
-// isHeCome = "Jupeter";
-var hisBag; // 이렇게 선언된경우 이후에 어떤 데이터 타입이라도 다 들어갈 수 있다.
-hisBag = 35;
-hisBag = "Name";
-// 하지만 ts는 이렇게 선언하는것을 권장
-var secondName = "Apple";
-var yourAge = 35;
-var yourBag;
-// let heroes = ["Superman", "Hulk", "Batman"]; -> 오직 배열 안에 문자열만 들어갈 수 있다.
-// let heroes: any = ["Superman", "Hulk", "Batman"]; -> 배열이든 아니든 모든 값이 다 들어갈 수 있다.
-// let heroes: any[] = ["Superman", "Hulk", "Batman"]; -> heroes에 배열만 할당할 수 있다.
-var heroes = ["Superman", "Hulk", "Batman"]; // 모든 값을 다 할당할 수 있다.
-heroes = 50;
-// Tuple
-var classB = ["Students", 20];
-classB = ["Teachers", 10];
-console.log(classB[0]);
-// enum
-var Color;
-(function (Color) {
-    Color[Color["Blue"] = 0] = "Blue";
-    Color[Color["Green"] = 10] = "Green";
-    Color[Color["Red"] = 11] = "Red";
-})(Color || (Color = {}));
-var color1 = Color.Green;
-console.log(Color.Blue);
-// any
-// any를 쓰면 TS를 쓰는 의미가 없어짐. 웬만하면 안쓴다.
-var batmanCar = "K5";
-console.log(batmanCar);
-batmanCar = { maker: "KIA", manufactured: 2017 };
-console.log(batmanCar);
-// function return type
-function returnMyName() {
-    return myName;
-}
-console.log(returnMyName());
-// function argument types
-function multiply(value1, value2) {
-    return value1 * value2;
-}
-console.log(multiply(2, 5));
-// Function Types
-var superMultiply;
-superMultiply = multiply;
-var resultMultiply = multiply(4, 6);
-// superMultiply = returnMyName;
-console.log(resultMultiply);
-console.log(superMultiply);
-console.log(superMultiply(2, 20));
-var heroOne = {
-    name: "Batman",
-    age: 25,
-    address: "서울특별시 강남구 역삼동"
+var bankAccount = {
+    money: 0,
+    deposit: function (value) {
+        this.money += value;
+    }
 };
-var heroTwo = {
+var user_1 = {
     name: "Superman",
-    age: 25,
-    address: "서울특별시 서초구 방배동"
+    bankAccount: bankAccount,
+    hobbies: ["Flying", "Guitar"]
 };
-var heroThree = {
-    name: "Ironman",
-    age: 25,
-    address: "서울특별시 광진구 군자동"
+var user_2 = {
+    name: "Batman",
+    bankAccount: bankAccount,
+    hobbies: ["Flying", "Guitar"]
 };
-// let hero2One: heroName2 = {
-//   name: "Batman",
-//   age: 25,
-//   address: "서울특별시 강남구 역삼동"
-// };
-// Union types
-var heroTeam = ["Superman", "Batman"];
-heroTeam = [101, 202];
-heroTeam = [true, false];
-// Check types = typeof
-var finalWinner = "Batman";
-if (finalWinner === "Superman") {
-    console.log(finalWinner);
-}
-if (typeof finalWinner == "string") {
-    console.log(finalWinner);
-}
+var user_3 = {
+    name: "Hulk",
+    bankAccount: bankAccount,
+    hobbies: ["halloween Party", "Climbing a building"]
+};
+user_1.bankAccount.deposit(1500);
+user_2.bankAccount.deposit(500);
+user_3.bankAccount.deposit(2500);
+user_1.bankAccount.deposit(3500);
+var totalBalance = bankAccount.money;
+console.log(totalBalance);
--- main.ts
+++ main.ts
@@ -1,115 +1,43 @@
-let myName: string;
-myName = "Banana";
-// myName = 35; //(이미 myName에 string이 할당되었기 때문에 이후에 number 할당 불가)
-
-let myAge = 25;
-// myAge = '35';
-
-let isHeCome = false;
-// isHeCome = "Jupeter";
-
-let hisBag; // 이렇게 선언된경우 이후에 어떤 데이터 타입이라도 다 들어갈 수 있다.
-hisBag = 35;
-hisBag = "Name";
-
-// 하지만 ts는 이렇게 선언하는것을 권장
-let secondName: string = "Apple";
-let yourAge: number = 35;
-let yourBag: any;
-
-// let heroes = ["Superman", "Hulk", "Batman"]; -> 오직 배열 안에 문자열만 들어갈 수 있다.
-// let heroes: any = ["Superman", "Hulk", "Batman"]; -> 배열이든 아니든 모든 값이 다 들어갈 수 있다.
-// let heroes: any[] = ["Superman", "Hulk", "Batman"]; -> heroes에 배열만 할당할 수 있다.
-let heroes: any = ["Superman", "Hulk", "Batman"]; // 모든 값을 다 할당할 수 있다.
-heroes = 50;
-
-// Tuple
-let classB: [string, number] = ["Students", 20];
-classB = ["Teachers", 10];
-console.log(classB[0]);
-
-// enum
-enum Color {
-  Blue,
-  Green = 10,
-  Red
-}
-
-let color1 = Color.Green;
-console.log(Color.Blue);
-
-// any
-// any를 쓰면 TS를 쓰는 의미가 없어짐. 웬만하면 안쓴다.
-let batmanCar: any = "K5";
-console.log(batmanCar);
-
-batmanCar = { maker: "KIA", manufactured: 2017 };
-console.log(batmanCar);
-
-// function return type
-function returnMyName(): string {
-  return myName;
-}
-console.log(returnMyName());
-
-// function argument types
-function multiply(value1: number, value2: number): number {
-  return value1 * value2;
-}
-console.log(multiply(2, 5));
-
-// Function Types
-let superMultiply: (a: number, b: number) => number;
-
-superMultiply = multiply;
-let resultMultiply = multiply(4, 6);
-// superMultiply = returnMyName;
-
-console.log(resultMultiply);
-console.log(superMultiply);
-console.log(superMultiply(2, 20));
-
-// Types of Objects: Properties = key + value
-type heroName = { name: string; age: number; address: string };
-
-let heroOne: heroName = {
-  name: "Batman",
-  age: 25,
-  address: "서울특별시 강남구 역삼동"
+type account = {
+  money: number;
+  deposit(args: number): void;
 };
-let heroTwo: heroName = {
-  name: "Superman",
-  age: 25,
-  address: "서울특별시 서초구 방배동"
-};
-let heroThree: heroName = {
-  name: "Ironman",
-  age: 25,
-  address: "서울특별시 광진구 군자동"
+let bankAccount: account = {
+  money: 0,
+  deposit(value: number): void {
+    this.money += value;
+  }
 };
 
-type heroName2 = {
+type User = {
   name: string;
-  age: number;
-  address: (arg: string) => string;
+  bankAccount: account;
+  hobbies: string[];
 };
-// let hero2One: heroName2 = {
-//   name: "Batman",
-//   age: 25,
-//   address: "서울특별시 강남구 역삼동"
-// };
 
-// Union types
-let heroTeam: string[] | number[] | boolean[] = ["Superman", "Batman"];
-heroTeam = [101, 202];
-heroTeam = [true, false];
+let user_1: User = {
+  name: "Superman",
+  bankAccount: bankAccount,
+  hobbies: ["Flying", "Guitar"]
+};
 
-// Check types = typeof
-let finalWinner = "Batman";
+let user_2: User = {
+  name: "Batman",
+  bankAccount: bankAccount,
+  hobbies: ["Flying", "Guitar"]
+};
 
-if (finalWinner === "Superman") {
-  console.log(finalWinner);
-}
-if (typeof finalWinner == "string") {
-  console.log(finalWinner);
-}
+let user_3: User = {
+  name: "Hulk",
+  bankAccount: bankAccount,
+  hobbies: ["halloween Party", "Climbing a building"]
+};
+
+user_1.bankAccount.deposit(1500);
+user_2.bankAccount.deposit(500);
+user_3.bankAccount.deposit(2500);
+user_1.bankAccount.deposit(3500);
+
+let totalBalance: number = bankAccount.money;
+
+console.log(totalBalance);
